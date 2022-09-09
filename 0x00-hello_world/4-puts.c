1617044b32ed9f567de75a3223611a4fe3e5c724
Ifezue Obinna <gentlespirit4real@gmail.com>
2022-09-09 00:02:11 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *   * Return: 0 if exited properly, non-zero otherwise
+ *  * Return: 0 if exited properly, non-zero otherwise
  *
  *    */
 
@@ -14,8 +14,8 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
